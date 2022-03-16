301d4c25b200586480aef2256c40868ec6c30f97
Davi Luiz <davilucena222@gmail.com>
2022-03-16 09:10:22 -0300

    typescript updates

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -5,8 +5,14 @@ import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss';
 
+interface Repository {
+    name: string;
+    description: string;
+    html_url: string;
+}
+
 export function RepositoryList() {
-    const [repositories, setRepositories] = useState([]);
+    const [repositories, setRepositories] = useState<Repository[]>([]);
 
     useEffect(() => {
         fetch('https://api.github.com/orgs/rocketseat/repos')
@@ -14,13 +20,14 @@ export function RepositoryList() {
         .then(data => setRepositories(data))
     }, [])
 
+
     return(
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
 
             <ul>
                 {repositories.map(repository => {
-                    return <RepositoryItem key={repository.name} repository={repository} />
+                    return <RepositoryItem key={repository.name} repository={repository}/>
                 })}
             </ul>
         </section>
